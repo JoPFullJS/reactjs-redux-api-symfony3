fa56e77bc2689baa54a46a0d23b5f3223bce2e69
JoPFullJS <jophalupi@gmail.com>
2017-07-22 22:50:50 +0200

    Definition des types d'actions

---
+++ src/constants/actionTypes.js
@@ -0,0 +1,12 @@
+//Asyncrone request and Promise based HTTP client for the browser
+import axios from 'axios';
+
+//Liste des Interventins 
+export const FETCH_INTERVENTIONS = 'FETCH_INTERVENTIONS';
+export const FETCH_INTERVENTIONS_SUCCESS = 'FETCH_INTERVENTIONS_SUCCESS';
+export const FETCH_INTERVENTIONS_FAILURE = 'FETCH_INTERVENTIONS_FAILURE';
+
+//Liste des Competences
+export const FETCH_COMPETENCES = 'FETCH_COMPETENCES';
+export const FETCH_COMPETENCES_SUCCESS = 'FETCH_COMPETENCES_SUCCESS';
+export const FETCH_COMPETENCES_FAILURE = 'FETCH_COMPETENCES_FAILURE';
